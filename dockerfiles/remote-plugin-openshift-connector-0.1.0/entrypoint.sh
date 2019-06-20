38820d960c0551f8699be49f2017870adb9a1f1a
Masaki Muranaka <monaka@monami-ya.com>
2019-06-20 02:39:01 +0000

    Refactor passwd/group related processes.
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>

--- dockerfiles/remote-plugin-openshift-connector-0.1.0/entrypoint.sh
+++ dockerfiles/remote-plugin-openshift-connector-0.1.0/entrypoint.sh
@@ -18,12 +18,12 @@ if ! grep -Fq "${USER_ID}" /etc/passwd; then
     # current user is an arbitrary
     # user (its uid is not in the
     # container /etc/passwd). Let's fix that
-    cat ${HOME}/passwd.template | \
+    cat /.passwd.template | \
     sed "s/\${USER_ID}/${USER_ID}/g" | \
     sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
     sed "s/\${HOME}/\/home\/theia/g" > /etc/passwd
 
-    cat ${HOME}/group.template | \
+    cat /.group.template | \
     sed "s/\${USER_ID}/${USER_ID}/g" | \
     sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
     sed "s/\${HOME}/\/home\/theia/g" > /etc/group
